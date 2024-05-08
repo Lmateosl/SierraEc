063d14b300adf6f25902e6e8ade85896ebbd68b2
mateo <mateosl@enerwicorp.com>
2024-05-07 21:33:51 -0500

    cambios anadidos de los demas idiomas

--- lenguajes/en/enInicio.js
+++ lenguajes/en/enInicio.js
@@ -1,51 +1,58 @@
 const inicio = {
     inicio: "HOME",
-    paquetes: "PACKAGES",
+    paquetes: "DESTINATIONS",
     nosotros: "ABOUT US",
     contactanos: "CONTACT US",
     botonReservaAhora: "Book Now",
     btnLang: "",
     "welcomeMessage": "Feel the joy of life, start a new journey!",
-    "subtitloWelcomeMessage": "Ecuador is a mega diverse country, full of magic in its landscapes, with lakes, lagoons, mountains, volcanoes, beaches, rivers, and jungles. A pluricultural country, with customs, traditions, and knowledge; a country with exquisite gastronomy and much more. Are you ready to explore Ecuador?",
-    "botonViajamos": "LET'S TRAVEL?",
-    "pasosMensaje": "THREE STEPS TO START YOUR JOURNEY",
+    "subtitloWelcomeMessage": "Discover the wonder of Ecuador! Immerse yourself in a world of unparalleled diversity, where every corner surprises you with its natural charm. From majestic volcanoes to dreamy beaches, through lush jungles and a rich cultural heritage. Enjoy our unique gastronomy and let yourself be captivated by the magic that only Ecuador can offer. Are you ready for an unforgettable adventure?",
+    "botonViajamos": "TRAVEL WITH US?",
+    "pasosMensaje": "Prepare your adventure in three simple steps!",
     "pasos": [
-        "Choose the destination(s) you want to visit on your trip to Ecuador.",
-        "Be clear about how many days you can allocate to your trip and how many people will be in your group.",
-        "Contact us through our customer service channels, and we will send you a customized travel proposal just for you."
+        "Select the destinations you want to explore on your trip through Ecuador.",
+        "Plan the duration of your adventure and let us know how many companions will be with you.",
+        "Contact us today and we will send you a personalized proposal for your dream trip!"
     ],
     "aDondeIr": "WHERE TO GO?",
-    "destinosTop": "TOP DESTINATIONS",
+    "destinosTop": "Discover the Best Destinations",
     btnReserva: "Book now!",
-    "vuelaPorEcuador": ["Fly around Ecuador",
-    "4 regions in one trip: \nGalapagos, Coast, Sierra, and Amazon."],
-    "paquetesIncluyen": ["Our packages include",
-    "Complete menu-type meals, transfers to excursions and activities, authorized guide, and much more."],
+    "vuelaPorEcuador": ["Discover Ecuador on a unique journey",
+    "Live an unforgettable adventure traveling through the country's four regions:\nGalapagos, Coast, Sierra, and Amazon."],
+    "paquetesIncluyen": ["Complete experience! Our packages include:",
+    "We offer unique adventures, with full menu-type meals, transfers to excursions and activities, authorized guide, and much more."],
     "noEsperesMas": ["We are SierraEC",
-    "Ecuador is a mega diverse country, and through our specialized tourism packages, you will be able to discover every corner.",
-    "Don't wait any longer and escape to your next adventure!"],
+    "Ecuador is a mega diverse country, and through our specialized tourism packages, you can explore every corner.",
+    "Take advantage now and embark on your next adventure!"],
     "tripsGallery": ["TRIPS GALLERY",
         "Our Destinations"],
+    cardsDestinos: {
+        destinosTop: ["Top Destinations", "Explore our top destinations and discover unforgettable experiences worldwide."],
+        ecuador: ["Ecuador", "Immerse yourself in the natural beauty and rich culture of Ecuador, from its mountains to its tropical beaches."],
+        galapagos: ["Galapagos", "Discover the Galapagos Islands, a unique paradise full of extraordinary wildlife and wonderful landscapes."],
+        turquia: ["Turkey", "Embark on an adventure through Turkey, where ancient history blends with modernity in a land full of wonders."],
+        btn: "See Destinations"
+    },
     "galerias": {
         "SantaCruz": [
             "Santa Cruz | Galapagos",
-            "The most well-known island in Galapagos as it is the main operations base and from where it is very easy to move to other areas. In Santa Cruz, you can do all kinds of activities such as snorkeling, diving, trekking, cycling, etc. Among its main tourist attractions are the Famous Cracks; the best beach on the islands Tortuga Bay with crystal clear waters, the Charles Darwin Station, and more!!",
-            "More"
+            "The best-known island in Galapagos as it is the base of operations and from where it is very easy to move to other areas. In Santa Cruz, it is possible to do all kinds of activities such as snorkeling, diving, trekking, biking, etc. Among its main tourist attractions, we have the Famous Cracks; the best beach in the Tortuga Bay islands with crystal clear waters, the Charles Darwin station, and more!",
+            "Read more"
         ],
         "SanCristobal": [
             "San Cristobal | Galapagos",
-            "One of the most fascinating islands within the Galapagos National Park. Its high sea tours are the most famous since you can reach the León Dormido, an iconic islet where you can snorkel with stingrays!! You can also visit Junco Lagoon, the only source of freshwater on the island, and more!",
-            "More"
+            "One of the most fascinating islands within the Galapagos National Park. Its high sea tours are the most famous since you can reach the Sleeping Lion, an iconic islet where you can snorkel with manta rays!! You can also visit Junco Lagoon, the only source of freshwater on the island, and more!",
+            "Read more"
         ],
         "Cuyabeno": [
             "Cuyabeno",
-            "Recognized as a wildlife production reserve for being the area with the highest number of endemic animal species in Ecuador. Getting to the heart of the reserve is quite an adventure as we will have to navigate its river while we venture into the Amazon jungle!",
-            "More"
+            "Recognized as a faunistic production reserve for being the area where the highest number of endemic animal species in Ecuador is found. Getting to the heart of the reserve is quite an adventure as we will have to navigate its river as we delve into the Amazon jungle!",
+            "Read more"
         ]
     },
     "cuatroMundos": [
-        "FOUR \nWORLDS",
-        "Ecuador is known for its geographical and climatic diversity, often described as “the four worlds” due to its four main regions:"
+        "Discover Ecuador's Four Worlds!",
+        "Welcome to Ecuador, where diversity is our calling card! Immerse yourself in a country that dazzles with its four worlds: the majestic Sierra, the mysterious charm of the Amazon, the golden coasts of the Coast, and the amazing Galapagos Islands. Get ready to live unique experiences in every corner of this land full of magic and adventure!"
     ],
     "cuatroMundosLista":[
         "SIERRA",
@@ -53,24 +60,24 @@ const inicio = {
         "COAST",
         "GALAPAGOS ISLANDS"
     ],
-    "botonExploro": "Explore!",
+    "botonExploro": "Explore it!",
     "testimoniosTitulo": [
         "100% SATISFIED PASSENGERS",
         "Testimonials"
     ],
     "testimonios": [
-        "“My experience with SierraEC was exceptional. The personalized attention, the quality of the tour guides, and the choice of places to visit were simply perfect. We not only explored impressive landscapes but also learned a lot about the local culture.\nWithout a doubt, this tour operator exceeded all my expectations, and I highly recommend it to all travelers looking for an unforgettable experience.”",
-        "“Dear members of the travelers' group, I want to express, on behalf of myself and my children, our sincere gratitude for the friendship that started from Galapagos to Istanbul and that we carry in our hearts the pleasant moments shared during the trip. We will always remember you and hope to stay in touch through this medium.\nThanks to Guido and Kiki for allowing us to share this adventure that hopefully we can repeat with this group elsewhere. A big hug to everyone.”"
+        "“My experience with SierraEC was exceptional. The personalized attention, the quality of the tour guides, and the choice of places to visit were simply perfect. We not only explored breathtaking landscapes but also learned a lot about the local culture.\nWithout a doubt, this tour operator exceeded all my expectations, and I highly recommend it to all travelers looking for an unforgettable experience.”",
+        "“Dear members of the travel group, I want to express, on behalf of myself and my children, our sincere gratitude for the friendship that began from Galapagos to Istanbul and that we carry in our hearts the pleasant moments shared during the trip. We will always remember you and hope to stay in touch through this medium.\nThanks to Guido and Kiki for allowing us to share this adventure that we hope to repeat with this group elsewhere. A big hug to everyone.”"
     ],
     footer: {
         "secciones": [
             {
                 "titulo": "Galapagos Islands",
-                "texto": "A natural treasure in the middle of the Ecuadorian Pacific.\nWith its unique biodiversity and breathtaking landscapes, these islands offer an unforgettable travel experience.\nThis corner of the world is a testimony to the beauty and fragility of our planet, and its conservation is essential for future generations."
+                "texto": "A natural treasure in the middle of the Ecuadorian Pacific.\nWith its unique biodiversity and impressive landscapes, these islands offer an unforgettable travel experience.\nThis corner of the world is a testimony to the beauty and fragility of our planet, and its conservation is essential for future generations."
             },
             {
                 "titulo": "Glampings",
-                "texto": "Glampings in Ecuador offer a unique luxury experience in the middle of nature. Imagine camping in luxurious tents with comfortable beds, private bathrooms, and gourmet cuisine, all while enjoying the natural beauty of Ecuador.\nGlampings are the perfect option for those who want to connect with nature without giving up modern comforts."
+                "texto": "Glampings in Ecuador offer a unique luxury experience amidst nature. Imagine camping in luxurious tents with comfortable beds, private bathrooms, and a gourmet kitchen, all while enjoying Ecuador's natural beauty.\nGlampings are the perfect choice for those who want to connect with nature without giving up modern comforts."
             },
             {
                 "titulo": "Amazon",
@@ -101,3 +108,4 @@ const inicio = {
 }
 
 export default inicio;
+
--- lenguajes/ger/gerInicio.js
+++ lenguajes/ger/gerInicio.js
@@ -1,90 +1,97 @@
 const inicio = {
     inicio: "START",
-    paquetes: "PAKETE",
+    paquetes: "ZIELE",
     nosotros: "ÜBER UNS",
     contactanos: "KONTAKTIERE UNS",
     botonReservaAhora: "Jetzt buchen",
     btnLang: "",
-    "welcomeMessage": "Fühlen Sie die Freude am Leben, beginnen Sie eine neue Reise!",
-    "subtitloWelcomeMessage": "Ecuador ist ein megadiverses Land, voller Magie in seinen Landschaften, mit Seen, Lagunen, Bergen, Vulkanen, Stränden, Flüssen und Wäldern. Ein plurikulturelles Land mit Bräuchen, Traditionen und Wissen; ein Land mit exquisiter Gastronomie und vielem mehr. Traust du dich, Ecuador kennenzulernen?",
+    "welcomeMessage": "Fühle die Freude am Leben, starte eine neue Reise!",
+    "subtitloWelcomeMessage": "Entdecke das Wunder von Ecuador! Tauche ein in eine Welt der unvergleichlichen Vielfalt, in der jede Ecke mit ihrem natürlichen Charme überrascht. Von majestätischen Vulkanen bis hin zu traumhaften Stränden, über üppige Dschungel und reiches kulturelles Erbe. Genieße unsere einzigartige Gastronomie und lass dich von der Magie verzaubern, die nur Ecuador bieten kann. Bist du bereit für ein unvergessliches Abenteuer?",
     "botonViajamos": "REISEN WIR?",
-    "pasosMensaje": "DREI SCHRITTE, UM IHRE REISE ZU BEGINNEN",
+    "pasosMensaje": "Bereite dein Abenteuer in drei einfachen Schritten vor!",
     "pasos": [
-        "Wählen Sie die Ziele aus, die Sie auf Ihrer Reise durch Ecuador kennenlernen möchten.",
-        "Klären Sie, wie viele Tage Sie für Ihre Reise verwenden können und wie viele Personen Ihre Gruppe bilden werden.",
-        "Schreiben Sie uns über unsere Kontaktkanäle an, und wir senden Ihnen einen maßgeschneiderten Reisevorschlag nur für Sie."
+        "Wähle die Ziele aus, die du auf deiner Reise durch Ecuador erkunden möchtest.",
+        "Plane die Dauer deines Abenteuers und teile uns mit, wie viele Begleiter dabei sein werden.",
+        "Kontaktiere uns noch heute, und wir senden dir einen personalisierten Vorschlag für deine Traumreise!"
     ],
-    "aDondeIr": "WO GEHEN WIR HIN?",
-    "destinosTop": "TOP-ZIELE",
+    "aDondeIr": "WOHIN GEHEN?",
+    "destinosTop": "Entdecke die besten Ziele",
     btnReserva: "Jetzt buchen!",
-    "vuelaPorEcuador": ["Rund um Ecuador fliegen", 
-    "4 Regionen in einer einzigen Reise: \nGalápagos, Küste, Sierra und Amazonien."],
-    "paquetesIncluyen": ["Unsere Pakete beinhalten",
-    "Vollständige Menüverpflegung, Transfers zu Ausflügen und Aktivitäten, autorisierte Führung und vieles mehr."],
+    "vuelaPorEcuador": ["Entdecke Ecuador auf einer einzigartigen Reise",
+    "Erlebe ein unvergessliches Abenteuer, indem du durch die vier Regionen des Landes reist:\nGalápagos, Küste, Sierra und Amazonas."],
+    "paquetesIncluyen": ["Vollständige Erfahrung! Dies beinhaltet unsere Pakete:",
+    "Wir bieten einzigartige Abenteuer mit vollständiger Menüverpflegung, Transfers zu Ausflügen und Aktivitäten, autorisierten Reiseleitern und vielem mehr."],
     "noEsperesMas": ["Wir sind SierraEC",
-    "Ecuador ist ein mega diverses Land, und mit unseren spezialisierten Tourismuspaketen können Sie jeden Winkel erkunden.",
-    "Warten Sie nicht länger und entfliehen Sie Ihrem nächsten Abenteuer!"],
+    "Ecuador ist ein megadiverses Land, und durch unsere spezialisierten Tourismus-Pakete kannst du jeden Winkel erkunden.",
+    "Nutze jetzt die Gelegenheit und begebe dich auf dein nächstes Abenteuer!"],
     "tripsGallery": ["REISEGALERIE",
         "Unsere Ziele"],
+    cardsDestinos: {
+        destinosTop: ["Top-Ziele", "Entdecke unsere Top-Ziele und erlebe unvergessliche Erlebnisse weltweit."],
+        ecuador: ["Ecuador", "Tauche ein in die natürliche Schönheit und reiche Kultur Ecuadors, von seinen Bergen bis zu seinen tropischen Stränden."],
+        galapagos: ["Galápagos", "Entdecke die Galápagos-Inseln, ein einzigartiges Paradies voller außergewöhnlicher Tierwelt und wunderschöner Landschaften."],
+        turquia: ["Türkei", "Begib dich auf ein Abenteuer durch die Türkei, wo sich alte Geschichte mit Modernität in einem Land voller Wunder vermischt."],
+        btn: "Ziele anzeigen"
+    },
     "galerias": {
         "SantaCruz": [
             "Santa Cruz | Galápagos",
-            "Die bekannteste Insel auf den Galápagosinseln, da sie das Basislager ist und es von dort aus sehr einfach ist, sich zu anderen Gebieten zu bewegen. Auf Santa Cruz sind alle Arten von Aktivitäten wie Schnorcheln, Tauchen, Wandern, Fahrradfahren usw. möglich. Zu den Hauptattraktionen gehören die berühmten Risse; der beste Strand der Inseln Tortuga Bay mit kristallklarem Wasser, die Charles Darwin Station und mehr!",
-            "Weiterlesen"
+            "Die bekannteste Insel in Galápagos, da sie das Hauptoperationszentrum ist und von wo aus es sehr einfach ist, sich in andere Gebiete zu bewegen. In Santa Cruz können alle Arten von Aktivitäten wie Schnorcheln, Tauchen, Trekking, Radfahren usw. durchgeführt werden. Zu den wichtigsten Sehenswürdigkeiten gehören die berühmten Risse; der beste Strand der Tortuga Bay-Inseln mit kristallklarem Wasser, die Charles Darwin-Station und mehr!",
+            "Mehr lesen"
         ],
         "SanCristobal": [
             "San Cristobal | Galápagos",
-            "Eine der faszinierendsten Inseln im Galapagos-Nationalpark. Die Bootsfahrten auf hoher See sind die berühmtesten, da Sie zum Leon Dormido gelangen können, einem markanten Felsen, an dem Sie mit Stachelrochen schnorcheln können! Sie können auch die Laguna de Junco besuchen, die einzige Süßwasserquelle auf der Insel, und mehr!",
-            "Weiterlesen"
+            "Eine der faszinierendsten Inseln im Galapagos-Nationalpark. Seine Hochseetouren sind die berühmtesten, da du zum Sleeping Lion gelangen kannst, einer ikonischen Insel, auf der du mit Mantarochen schnorcheln kannst!! Du kannst auch die Laguna de Junco besuchen, die einzige Quelle für Süßwasser auf der Insel, und mehr!",
+            "Mehr lesen"
         ],
         "Cuyabeno": [
             "Cuyabeno",
-            "Als ein Faunenproduktionsreservat anerkannt, da es die Region ist, in der die größte Anzahl endemischer Tierarten in Ecuador vorkommt. Das Erreichen des Herzens des Reservats ist ein echtes Abenteuer, da wir auf seinem Fluss navigieren müssen, während wir uns in den Amazonasdschungel wagen!",
-            "Weiterlesen"
+            "Anerkannt als Faunaproduktionsreservat, da hier die größte Anzahl endemischer Tierarten in Ecuador zu finden ist. Die Fahrt zum Herzen des Reservats ist ein echtes Abenteuer, da wir auf seinem Fluss navigieren müssen, während wir uns in den Amazonas-Dschungel begeben!",
+            "Mehr lesen"
         ]
     },
     "cuatroMundos": [
-        "VIER \nWELTEN",
-        "Ecuador ist bekannt für seine geografische und klimatische Vielfalt, die oft als „die vier Welten“ beschrieben wird, aufgrund seiner vier Hauptregionen:"
+        "Entdecke die vier Welten Ecuadors!",
+        "Willkommen in Ecuador, wo die Vielfalt unsere Visitenkarte ist! Tauche ein in ein Land, das mit seinen vier Welten fasziniert: der majestätischen Sierra, dem mysteriösen Charme des Amazonas, den goldenen Küsten der Küste und den erstaunlichen Galápagos-Inseln. Mach dich bereit für einzigartige Erlebnisse in jeder Ecke dieses Landes voller Magie und Abenteuer!"
     ],
     "cuatroMundosLista":[
         "SIERRA",
-        "AMAZONIEN",
+        "AMAZONAS",
         "KÜSTE",
         "GALÁPAGOS-INSELN"
     ],
-    "botonExploro": "Entdecken Sie es!",
+    "botonExploro": "Erkunden!",
     "testimoniosTitulo": [
-        "100% ZUFRIEDENE REISENDER",
-        "Referenzen"
+        "100% ZUFRIEDENE PASSAGIERE",
+        "Kundenbewertungen"
     ],
     "testimonios": [
-        "„Meine Erfahrung mit SierraEC war außergewöhnlich. Die persönliche Betreuung, die Qualität der Reiseleiter und die Auswahl der zu besuchenden Orte waren einfach perfekt. Wir haben nicht nur beeindruckende Landschaften erkundet, sondern auch viel über die lokale Kultur gelernt.\nDiese Reiseagentur hat zweifellos alle meine Erwartungen übertroffen, und ich empfehle sie allen Reisenden, die nach einem unvergesslichen Erlebnis suchen.“",
-        "„Liebe Mitglieder der Reisegruppe, ich möchte Ihnen im Namen meiner Kinder und mir unseren aufrichtigen Dank für die Freundschaft ausdrücken, die von Galapagos bis nach Istanbul begann und die wir in unseren Herzen tragen und die angenehmen Momente während der Reise geteilt haben. Wir werden uns immer daran erinnern und hoffen, weiterhin über diese Plattform in Kontakt zu bleiben.\nDanke an Guido und Kiki, dass wir dieses Abenteuer teilen durften, das wir hoffentlich mit dieser Gruppe an einen anderen Ort wiederholen können. Eine Umarmung an alle.“"
+        "„Meine Erfahrung mit SierraEC war außergewöhnlich. Die persönliche Betreuung, die Qualität der Reiseleiter und die Auswahl der Besichtigungsorte waren einfach perfekt. Wir haben nicht nur atemberaubende Landschaften erkundet, sondern auch viel über die lokale Kultur gelernt.\nDiese Reiseagentur hat definitiv alle meine Erwartungen übertroffen, und ich kann sie allen Reisenden wärmstens empfehlen, die ein unvergessliches Erlebnis suchen.“",
+        "„Liebe Mitglieder der Reisegruppe, ich möchte im Namen meiner Kinder und mir unseren aufrichtigen Dank für die Freundschaft ausdrücken, die von Galapagos bis Istanbul begonnen hat und die wir in unseren Herzen tragen die angenehmen Momente, die wir während der Reise geteilt haben. Wir werden euch immer in Erinnerung behalten und hoffen, über dieses Medium in Kontakt zu bleiben.\nDanke an Guido und Kiki, dass sie uns ermöglicht haben, dieses Abenteuer zu teilen, das wir hoffentlich mit dieser Gruppe an einem anderen Ort wiederholen können. Eine Umarmung an euch alle.“"
     ],
     footer: {
         "secciones": [
             {
                 "titulo": "Galápagos-Inseln",
-                "texto": "Ein natürlicher Schatz mitten im ecuadorianischen Pazifik.\nMit ihrer einzigartigen Biodiversität und atemberaubenden Landschaften bieten diese Inseln eine unvergessliche Reiseerfahrung.\nDieser Teil der Welt ist ein Zeugnis für die Schönheit und Zerbrechlichkeit unseres Planeten, und ihre Erhaltung ist für zukünftige Generationen von entscheidender Bedeutung."
+                "texto": "Ein Naturjuwel mitten im ecuadorianischen Pazifik.\nMit ihrer einzigartigen Biodiversität und beeindruckenden Landschaften bieten diese Inseln ein unvergessliches Reiseerlebnis.\nDieser Teil der Welt ist ein Zeugnis für die Schönheit und Fragilität unseres Planeten, und ihr Schutz ist entscheidend für zukünftige Generationen."
             },
             {
                 "titulo": "Glampings",
-                "texto": "Die Glampings in Ecuador bieten eine einzigartige Luxuserfahrung inmitten der Natur. Stellen Sie sich vor, Sie campen in luxuriösen Zelten mit bequemen Betten, eigenen Bädern und einer Gourmetküche, während Sie die natürliche Schönheit Ecuadors genießen.\nGlampings sind die perfekte Wahl für diejenigen, die sich mit der Natur verbinden möchten, ohne auf modernen Komfort zu verzichten."
+                "texto": "Glampings in Ecuador bieten ein einzigartiges Luxuserlebnis mitten in der Natur. Stell dir vor, du campst in luxuriösen Zelten mit bequemen Betten, privaten Badezimmern und einer Gourmetküche, während du die natürliche Schönheit Ecuadors genießt.\nGlampings sind die perfekte Wahl für diejenigen, die sich mit der Natur verbinden möchten, ohne auf modernen Komfort zu verzichten."
             },
             {
-                "titulo": "Amazonien",
-                "texto": "Das ecuadorianische Amazonasgebiet bietet durch Aufenthalte in Lodges mitten im Dschungel eine perfekte Gelegenheit, magische Erlebnisse zu leben."
+                "titulo": "Amazonas",
+                "texto": "Der ecuadorianische Amazonas durch Aufenthalte in Lodges mitten im Dschungel ist die perfekte Gelegenheit, magische Erlebnisse und Momente zu erleben."
             },
             {
-                "titulo": "Haben Sie eine Reise im Kopf?",
-                "texto": "Wir sind ein Reiseveranstalter, der sich darauf konzentriert, das Beste von Ecuador zu zeigen."
+                "titulo": "Hast du eine Reise im Kopf?",
+                "texto": "Wir sind ein Reisebüro und Tourismusveranstalter, der sich darauf konzentriert, das Beste von Ecuador zu präsentieren."
             }
         ],
         "lastPart": [
             {
                 "titulo": "Adresse:",
-                "texto": "Agustín Agualongo y Francisco Tamariz, Cuenca - Ecuador."
+                "texto": "Agustín Agualongo und Francisco Tamariz, Cuenca - Ecuador."
             },
             {
                 "titulo": "Reservierungen 1",
