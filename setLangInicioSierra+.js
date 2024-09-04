0edbeb3163f6d3a14cb31217b80362498f1dda40
Lmateosl <mateosan67@hotmail..com>
2024-09-04 15:56:43 -0400

    Tiempo de slides modificado

--- setLangInicioSierra+.js
+++ setLangInicioSierra+.js
@@ -35,7 +35,7 @@ var swiper = new Swiper('.mySwiper', {
       clickable: true,
     },
     autoplay: {
-        delay: 5000,
+        delay: 3000,
         disableOnInteraction: false,
     },
     navigation: {
